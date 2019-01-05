d21665287d65ae6472f2d0dd4300b49d8098ca39
Panji_Wardhana <45580022+PanjiWardhana@users.noreply.github.com>
2019-01-05 14:20:55 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int p,l,luas;
+    cout<< "<==== Menghitung Luas Persegi Panjang ====>\n";
+    cout<< "Input Panjang :";
+    cin>>p;
+    cout<< "Input Lebar :";
+    cin>>l;
+    luas=p*l;
+    cout<< "Luas Persegi Panjang :"<<luas;
+    return 0;
+}
